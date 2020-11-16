0a6f577410d0703b1386ae791e3dc8c2f040610c
matespinosa <matespinosa09@gmail.com>
2020-11-16 08:20:31 -0500

    Terminando sidebar con ReactJs

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/reports" component={Reports} />
+          <Route path="/products" component={Products} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
